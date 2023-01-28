f0cdafa416a836c5eb0636f1eb0fc98b3f7bc128
Sergiu Grisca <sergiu@kuama.net>
2023-01-28 13:12:14 +0100

    :sparkles: Meta component

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,16 +1,17 @@
-import Head from "next/head";
+import { ContactForm } from "../components/form";
 import { Layout } from "../components/layout";
+import { Meta } from "../components/meta";
 
 export default function ContactsPage() {
   return (
     <>
-      <Head>
-        <title>Kotakt: Zmeu Hausbesorger</title>
-        <meta name="description" content="Handwerker Zmeu Petru" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>Contact form here</Layout>
+      <Meta
+        title="Kotakt: Zmeu Hausbesorger"
+        description="Kotakt: Zmeu Hausbesorger"
+      />
+      <Layout>
+        <ContactForm />
+      </Layout>
     </>
   );
 }
