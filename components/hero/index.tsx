12f75e035e9e57ee31ae3e5e9b0b9d3e2d77f930
Sergiu Grisca <sergiu@kuama.net>
2024-02-12 16:28:12 +0100

    💄 fix mobile styles

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -2,7 +2,7 @@ import { RoundImage } from "../roundImage";
 
 export const Hero = () => {
   return (
-    <section className="flex flex-col mb-20 px-5 text-center md:text-left md:flex-row-reverse md:items-center">
+    <section className="flex flex-col mb-20 pt-10 md:pt-0 px-5 text-center md:text-left md:flex-row-reverse md:items-center">
       <RoundImage />
       <div className="mt-[54px] max-w-[637px] md:mt-0">
         <h2 className="font-bold text-[45px] leading-[67px]">
